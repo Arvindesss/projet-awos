f4f67f05cb095f3c5607bf349bfa3c49268ca51c
Arvindesss <77747376+Arvindesss@users.noreply.github.com>
2025-06-28 18:22:59 +0200

    add payment service

---
+++ payment-service/src/main/java/com/dauphinesitn/payment_service/model/Payment.java
@@ -0,0 +1,25 @@
+package com.dauphinesitn.payment_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    private UUID paymentId;
+
+    private UUID customerId;
+
+    private double amount;
+
+    private String currency;
+}
---
+++ payment-service/src/main/java/com/dauphinesitn/payment_service/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,39 @@
+package com.dauphinesitn.payment_service.service.impl;
+
+import com.dauphinesitn.payment_service.dto.PaymentDTO;
+import com.dauphinesitn.payment_service.model.Payment;
+import com.dauphinesitn.payment_service.repository.PaymentRepository;
+import com.dauphinesitn.payment_service.service.PaymentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
+
+    @Override
+    public Payment getPaymentById(UUID id) {
+        return paymentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Payment not found with id: " + id));
+    }
+
+    @Override
+    public Payment createPayment(PaymentDTO payment) {
+        Payment newPayment = Payment.builder()
+                .paymentId(UUID.randomUUID())
+                .customerId(payment.customerId())
+                .amount(payment.amount())
+                .build();
+        return paymentRepository.save(newPayment);
+    }
+}
