1d65067c15804a1dce268010af87ce729887adf6
Arvindesss <77747376+Arvindesss@users.noreply.github.com>
2025-06-29 10:37:46 +0200

    to squash pricing service

--- pricing-service/src/main/java/com/dauphinesitn/pricing_service/dto/ItineraryPricingDTO.java
+++ pricing-service/src/main/java/com/dauphinesitn/pricing_service/dto/ItineraryPricingDTO.java
@@ -2,6 +2,11 @@ package com.dauphinesitn.pricing_service.dto;
 
 import lombok.Builder;
 
+import java.util.UUID;
+
 @Builder
-public class ItineraryPricingDTO {
+public record ItineraryPricingDTO (UUID itineraryPricingId,
+                                   UUID departureAirportId,
+                                   UUID arrivalAirportId,
+                                   double price) {
 }
